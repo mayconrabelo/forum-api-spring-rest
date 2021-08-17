f78a192027af5fa89ff5286800b58e25bc0f1390
Maycon Rabelo <loganrabelo@gmail.com>
2021-08-17 15:18:36 -0300

    Forum API Spring REST

---
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -0,0 +1,45 @@
+package br.com.alura.forum.controller.form;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+
+public class TopicoForm {
+
+	@NotNull @NotBlank @Length(min = 5)
+	private String titulo;
+	@NotNull @NotBlank @Length(min = 10)
+	private String mensagem;
+	@NotNull @NotBlank
+	private String nomeCurso;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+	public Topico converter(CursoRepository cursoRepository) {
+		Curso curso = cursoRepository.findByNome(nomeCurso);
+		return new Topico(titulo,mensagem,curso);
+	}
+	
+	
+}
