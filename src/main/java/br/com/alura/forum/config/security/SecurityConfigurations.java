7002dd72877990a734f9c5000b9d7716de3dbd2f
Maycon Rabelo <loganrabelo@gmail.com>
2021-08-24 10:26:59 -0300

    Spring boot teste profiles e deploy

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -3,6 +3,7 @@ package br.com.alura.forum.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -18,6 +19,7 @@ import br.com.alura.forum.repository.UsuarioRepository;
 
 @EnableWebSecurity
 @Configuration
+@Profile( value= {"prod","test","default"})
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
@@ -48,6 +50,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
 		.antMatchers(HttpMethod.POST, "/auth").permitAll()
 		.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+		.antMatchers(HttpMethod.DELETE, "/topicos/*").hasRole("MODERADOR")
 		.anyRequest().authenticated()
 		.and().csrf().disable()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,11 @@
 INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$6B6VzOYkxSh9RpNy0lXj2umNGOdUFKRC2FQpovN1Qz9OSM1Lvk1bK');
+INSERT INTO USUARIO(nome, email, senha) VALUES('moderador', 'moderador@email.com', '$2a$10$6B6VzOYkxSh9RpNy0lXj2umNGOdUFKRC2FQpovN1Qz9OSM1Lvk1bK');
+
+INSERT INTO PERFIL(id,nome) VALUES(1,'ROLE_ALUNO');
+INSERT INTO PERFIL(id,nome) VALUES(2,'ROLE_MODERADOR');
+
+INSERT INTO USUARIO_PERFIS(usuario_id,perfis_id) VALUES (1,1);
+INSERT INTO USUARIO_PERFIS(usuario_id,perfis_id) VALUES (2,2);
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
